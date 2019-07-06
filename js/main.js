ae41249733e7233695c25ae3d621586adb22732e
zhangyuanyuan <1031752581@qq.com>
2019-07-06 21:56:26 +0800

    protofilo

--- js/main.js
+++ js/main.js
@@ -6,10 +6,8 @@ window.onload  = function(){
 	// 头部元素初始高度
 	var headerNavHeight = headerNav.offsetHeight;
 
-    // 监听滚轮事件
-	listenMouseScroll();
-
 	// 监听滚轮事件
+	listenMouseScroll();
 	function listenMouseScroll(){
 
 		var scrollHeight = 0;//滚动条滚动的距离
@@ -27,6 +25,38 @@ window.onload  = function(){
 	}
 
 	//开启twitter轮播图
-	zqs.carousel();
+	twitterCarousel();
+	function twitterCarousel(){
+		zqs.carousel();
+	}
+	
+
+	//作品切换
+	portfolioChange();
+	function portfolioChange(){
+		var liArr = document.querySelectorAll('.portfolio .nav>ul>li')
+
+		// 给每个li添加点击事件
+		for(var i=0;i<liArr.length;i++){
+			liArr[i].index = i;
+			liArr[i].onclick = function(){
+				//切换li的active状态
+				for(var i=0;i<liArr.length;i++){
+					zqs.removeClass(liArr[i],'active')
+				}
+				zqs.addClass(liArr[this.index],'active')
+				// all
+				if(this.index == 0){
+
+				}else if (this.index == 1){
+					//photography
+				}else if (this.index == 2){
+					//UX DESIGN
+				}
+			}
+		}
+
+
+	}
 	
 }
