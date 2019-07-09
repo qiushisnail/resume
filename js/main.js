638b83ad090cc5c77c45b6774a7af407e8d1f2b9
qiushizhang <1031752581@qq.com>
2019-07-09 17:17:59 +0800

    优化头部导航代码

--- js/main.js
+++ js/main.js
@@ -3,29 +3,7 @@ setTimeout(function(){
 },2000)
 window.onload  = function(){
 
-    //获取头部元素 
-	var header = document.getElementById('header'); 
-	var headerNav = header.querySelector('.header-nav');
-	// 头部元素初始高度
-	var headerNavHeight = headerNav.offsetHeight;
-
-	// 监听滚轮事件
-	listenMouseScroll();
-	function listenMouseScroll(){
-
-		var scrollHeight = 0;//滚动条滚动的距离
-		
-		window.addEventListener('scroll',scrollFun,false);
-
-		function scrollFun(){			
-			// 只要滚动就改变样式
-			if(window.scrollY>0){
-				zqs.addClass(headerNav,'header-sticky');
-			}else{
-				zqs.removeClass(headerNav,'header-sticky');
-			}
-		}
-	}
+ 
 
 	//开启twitter轮播图
 	twitterCarousel();
---
+++ js/sticky-nav.js
@@ -0,0 +1,33 @@
+!function(){
+  /*固定头部*/
+  //获取头部导航元素
+	var view = header.querySelector('.header-nav');
+
+	var controller = {
+    view: null,
+    init: function(view){
+      this.view = view;
+      this.bindEvents();
+    },
+    bindEvents: function(){
+      window.addEventListener('scroll',()=>{
+        if(window.scrollY>0){
+          this.active();
+        }else{
+          this.deactive();
+        }
+      },false);
+      
+    },
+    active: function(){
+      zqs.addClass(this.view,'header-sticky');
+    },
+    deactive: function(){
+      zqs.removeClass(this.view,'header-sticky');
+    }
+
+  }
+
+  controller.init(view);
+	
+}.call()
